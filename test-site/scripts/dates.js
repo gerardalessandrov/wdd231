2c95911148b09e37e429618ab123226ddaff7e00
gerardalessandrov <gerardvigorodrigues@gmail.com>
2024-09-21 23:18:55 -0500

    changes

---
+++ test-site/scripts/dates.js
@@ -0,0 +1,10 @@
+
+
+
+// use the date object
+const today = new Date();
+const year = document.querySelector("#year");
+let alert = (document.lastModified);
+
+year.innerHTML = `©<span class="highlight">${today.getFullYear()} Gerard Vigo Rodrigues Trujillo</span>`;
+short.innerHTML = `Last Modification: <span class="highlight"> ${alert}</span>`;
