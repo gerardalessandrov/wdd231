a5107425ff56beabd258d1ac7fef9233bbd19b16
gerardalessandrov <gerardvigorodrigues@gmail.com>
2024-09-23 23:16:21 -0500

    changes

--- test-site/scripts/dates.js
+++ test-site/scripts/dates.js
@@ -1,10 +1,16 @@
-
-
-
 // use the date object
+const mainnav = document.querySelector(".navigation");
+const hambutton = document.querySelector("#menu");
+
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hambutton.addEventListener("click", () => {
+  mainnav.classList.toggle("show");
+  hambutton.classList.toggle("show");
+});
+
 const today = new Date();
 const year = document.querySelector("#year");
-let alert = (document.lastModified);
+let alert = document.lastModified;
 
 year.innerHTML = `©<span class="highlight">${today.getFullYear()} Gerard Vigo Rodrigues Trujillo</span>`;
 short.innerHTML = `Last Modification: <span class="highlight"> ${alert}</span>`;
